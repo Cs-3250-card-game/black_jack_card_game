64d4be0b70b0af5ec7b96c5c413cb5e0007cfefd
Tri Nguyen <112127702+imagirlrapper@users.noreply.github.com>
2023-05-02 20:58:08 -0700

    Update cardGame.py
    finished section

---
+++ cardGame.py
@@ -0,0 +1,73 @@
+from tkinter import *
+import random
+from PIL import Image, ImageTk
+import os
+
+root = Tk()
+root.title('Codemy.com - Card Deck')
+root.geometry("1200x800")
+root.configure(background="green")
+
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
+
+# Create Frames For Cards
+dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
+dealer_frame.pack(padx=20, ipadx=20)
+
+player_frame = LabelFrame(my_frame, text="Player", bd=0)
+player_frame.pack(ipadx=20, pady=10)
+
+# Put Dealer cards in frames
+dealer_label_1 = Label(dealer_frame, text='')
+dealer_label_1.grid(row=0, column=0, pady=20, padx=20)
+
+dealer_label_2 = Label(dealer_frame, text='')
+dealer_label_2.grid(row=0, column=1, pady=20, padx=20)
+
+dealer_label_3 = Label(dealer_frame, text='')
+dealer_label_3.grid(row=0, column=2, pady=20, padx=20)
+
+dealer_label_4 = Label(dealer_frame, text='')
+dealer_label_4.grid(row=0, column=3, pady=20, padx=20)
+
+dealer_label_5 = Label(dealer_frame, text='')
+dealer_label_5.grid(row=0, column=4, pady=20, padx=20)
+
+# Put Player cards in frames
+player_label_1 = Label(player_frame, text='')
+player_label_1.grid(row=1, column=0, pady=20, padx=20)
+
+player_label_2 = Label(player_frame, text='')
+player_label_2.grid(row=1, column=1, pady=20, padx=20)
+
+player_label_3 = Label(player_frame, text='')
+player_label_3.grid(row=1, column=2, pady=20, padx=20)
+
+player_label_4 = Label(player_frame, text='')
+player_label_4.grid(row=1, column=3, pady=20, padx=20)
+
+player_label_5 = Label(player_frame, text='')
+player_label_5.grid(row=1, column=4, pady=20, padx=20)
+
+# Create Button Frame
+button_frame = Frame(root, bg="green")
+button_frame.pack(pady=20)
+
+# Create a couple buttons
+shuffle_button = Button(button_frame, text="Shuffle Deck", font=("Helvetica", 14), command=shuffle)
+shuffle_button.grid(row=0, column=0)
+
+card_button = Button(button_frame, text="Hit Me!", font=("Helvetica", 14), command=player_hit)
+card_button.grid(row=0, column=1, padx=10)
+
+stand_button = Button(button_frame, text="Stand!", font=("Helvetica", 14))
+stand_button.grid(row=0, column=2)
+
+
+
+# Shuffle Deck On Start
+shuffle()
+
+
+root.mainloop()
